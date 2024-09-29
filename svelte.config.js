fdacde0fad7cf3e1d4bfa933d66d58055142a1b9
NvLL-Zen <nvll.zen@gmail.com>
2024-09-29 21:07:09 +0700

    Migrate to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,7 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
