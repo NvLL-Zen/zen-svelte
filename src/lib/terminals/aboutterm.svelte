f6b93e5ee0f7d29ee3ffebb2a95f8b35f5995ae2
NvLL-Zen <nvll.zen@gmail.com>
2024-10-10 15:04:18 +0700

    Added additional about, and added zenfetch and ls

--- src/lib/terminals/aboutterm.svelte
+++ src/lib/terminals/aboutterm.svelte
@@ -1,6 +1,6 @@
 <div class="main-container">
 <div class="term_div left-down">
-    <h2>$ cat about.txt</h2>
+    <h2 class="term_command">$ cat about_myself.txt</h2>
     <p>
         Hi there, the name's Fauzan Rizky Naufal. But you can just call me
         Ojan, i'm a highschool student in Indonesia. And since i was
@@ -12,6 +12,11 @@
         programming book in a library instead, and since then, it became my go to language.
     </p>
     <p>
+        From simple scripts to web scrapings, Python is my main. In web development
+        i'm still learning JS Frameworks such as Astro and Svelte aswell as 
+        things like Sass. And is looking forward to learn PHP and Laravel
+    </p>
+    <p>
         Aside from the world of programming, i usually unwind myself with playing
         videogames. Both videogames and programming have been my favorite things
         since i was a child, they have a special place in my heart that i will 
@@ -20,15 +25,31 @@
 </div>
 <div class="right-split-down">
     <div class="term_div half-down">
-        <h1>neofetch</h1>
+        <h2 class="term_command">$ zenfetch</h2>
+        <p class="zenfetch-p">
+            IMG
+        </p>
+        <p class="zenfetch-p">OS: Windows, Linux</p>
+        <p class="zenfetch-p">CPU: INTP</p>
+        <p class="zenfetch-p">Specialty: Python, Javascript</p>
+        <p class="zenfetch-p">Experience: 4 Years</p>
+        <p class="zenfetch-p">Nationality: Indonesian</p>
+        <p class="zenfetch-p">Language: Indonesian, English, Mandarin(Standard-Simplified)</p>
     </div>
     <div class="term_div half-down">
-        <h1>htop</h1>
+        <h2 class="term_command">$ ls ~/Personal/Projects</h2>
+        <p>/zen-svelte</p>
+        <p>/-nethack</p>
+        <p>/scefco</p>
+
     </div>
 </div>
 </div>
 
 <style>
+    /* * {
+        border: 1px solid red;
+    } */
     .main-container {
         width: 100%;
         height: 100%;
@@ -37,7 +58,7 @@
     }
     .term_div {
         box-sizing: border-box;
-        padding: 10px;
+        padding: 12.5px;
         border: 1px solid white;
         text-align: justify;
     }
@@ -52,5 +73,14 @@
 
     .right-split-down {
         width: 50%;
+    }
+    
+    .zenfetch-p {
+        margin: 0;
+    }
+
+    .term_command {
+        margin-top: 0;
+        margin-bottom: 8px;
     }
 </style>
